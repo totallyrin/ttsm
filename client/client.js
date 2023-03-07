1424fa559a49a8b415e3279b0b73755d7503a0b8
Lucy Woloszczuk <lucy.woloszczuk@gmail.com>
2023-03-07 14:55:16 -0500

    fixed button again

--- client/client.js
+++
@@ -1,8 +0,0 @@
-function togglePassword() {
-    var x = document.getElementById("password");
-    if (x.type === "password") {
-        x.type = "text";
-    } else {
-        x.type = "password";
-    }
-}
--- login.js
+++ login.js
@@ -2,6 +2,14 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-button");
 const loginErrorMsg = document.getElementById("login-error-msg-holder");
 
+function togglePassword() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
