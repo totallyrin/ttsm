8f276fe703aedea770667d0d5959f3801cccbf66
Lucy W <lucy.woloszczuk@gmail.com>
2023-03-07 14:29:05 -0500

    initial commit

---
+++ client/client.js
@@ -0,0 +1,8 @@
+function togglePassword() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
