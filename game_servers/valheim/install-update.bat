8b761a93a0c412bc41b14fed6bd2e0f7d024490b
Lucy Woloszczuk <lucy.woloszczuk@gmail.com>
2023-04-14 20:18:04 -0400

    added valheim server

---
+++ game_servers/valheim/install-update.bat
@@ -0,0 +1,3 @@
+@echo off
+
+steamcmd.exe +force_install_dir ".\." +login anonymous +app_update 896660 -beta none validate +quit
---
+++ game_servers/valheim/start-server.bat
@@ -0,0 +1,4 @@
+@echo off
+set SteamAppId=892970
+echo "Starting server PRESS CTRL-C to exit"
+"steamapps\common\Valheim dedicated server\valheim_server.exe" -nographics -batchmode -name "totallyrin" -port 2456 -world "world_1" -password "lucyw" -public 1
