cf4ea7ff5ffa4fcd979b2ced4f954babc67274b7
totallyrin <lucy.woloszczuk@gmail.com>
2023-04-16 16:12:02 -0400

    added dedicated steamcmd directory

---
+++ game_servers/steamcmd/update-pz.bat
@@ -0,0 +1,3 @@
+@echo off
+
+steamcmd.exe +force_install_dir "..\pz" +login anonymous +app_update 380870 validate +quit
---
+++ game_servers/steamcmd/update-valheim.bat
@@ -0,0 +1,3 @@
+@echo off
+
+steamcmd.exe +force_install_dir "..\valheim" +login anonymous +app_update 896660 -beta none validate +quit
