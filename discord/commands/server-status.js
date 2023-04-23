29f1a72d162d5d6af2ebc2a6ef5d0f37886d382d
totallyrin <lucy.woloszczuk@gmail.com>
2023-04-23 09:24:02 -0400

    updated server-status.js to have appealing formatting

--- discord/commands/server-status.js
+++ discord/commands/server-status.js
@@ -9,9 +9,9 @@ module.exports = {
     async execute(interaction) {
         let data = '';
         for (const server in servers.servers) {
-            data += `\n${server === 'pz' ? 'Project Zomboid' : server.charAt(0).toUpperCase() + server.slice(1)} : ${servers.servers[server].running ? 'online' : 'offline'}`;
+            data += `\n${(server === 'pz' ? 'Project Zomboid' : server.charAt(0).toUpperCase() + server.slice(1)).padEnd(15, ' ')} : ${servers.servers[server].running ? 'online' : 'offline'}`;
         }
-        await interaction.reply('\`\`\`SERVER STATUSES:' + data + '\`\`\`');
+        await interaction.reply('\`\`\`SERVER STATUSES :' + data + '\`\`\`');
     },
 };
 
