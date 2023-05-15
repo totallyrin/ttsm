ec67a41f2bc65ef2e3b8a45a8e88ecaddc243870
Lucy Woloszczuk <lucy.woloszczuk@gmail.com>
2023-05-15 09:56:37 -0400

    removed server-side code from repo

--- game_servers/minecraft/start-server.bat
+++
@@ -1,1 +0,0 @@
-java -Xmx1024M -Xms1024M -jar server.jar nogui
--- game_servers/valheim/start-server.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set SteamAppId=892970
-echo "Starting server PRESS CTRL-C to exit"
-"./valheim_server.exe" -nographics -batchmode -name "totallyrin" -port 2456 -world "server_1" -password "passwordpass"
--public 1
