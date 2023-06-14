e83bb05bacf7fe40cc199717569f4914355ee379
Lucy Woloszczuk <lucy.woloszczuk@gmail.com>
2023-06-14 14:10:54 -0400

    added performance logging statements to try and locate problem with api call

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,113 +1,123 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth, {NextAuthOptions} from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { url } from "../../../utils/utils";
+import {url} from "../../../utils/utils";
 
 async function attempt_login(
-  username,
-  password
+    username,
+    password
 ): Promise<{ id: string; username: string; role: string } | boolean> {
-  const ws = new WebSocket(url);
+    const ws = new WebSocket(url);
 
-  return await new Promise((resolve, reject) => {
-    let result, user, loginPromise;
-    ws.addEventListener("open", async () => {
-      ws.send(
-        JSON.stringify({
-          type: "login",
-          username: username,
-          password: password,
-        })
-      );
+    return await new Promise((resolve, reject) => {
+        let result, user, loginPromise;
+        const t0 = performance.now();
+        ws.addEventListener("open", async () => {
+            const t1 = performance.now();
+            console.log(`WebSocket connection opened in ${t1 - t0}ms`);
+            ws.send(
+                JSON.stringify({
+                    type: "login",
+                    username: username,
+                    password: password,
+                })
+            );
 
-      loginPromise = new Promise((resolve) => {
-        ws.onmessage = function (event) {
-          // get data from message
-          const data = JSON.parse(event.data);
-          // wait for login success message
-          if (data.type === "login") {
-            if (data.success) {
-              // User exists and password is correct
-              user = {
-                id: data.id,
-                username: data.username,
-                role: data.role,
-              };
-              resolve(user);
-            }
-            result = data.success;
-          }
-        };
-      });
+            loginPromise = new Promise((resolve) => {
+                const t2 = performance.now();
+                ws.onmessage = function (event) {
+                    // get data from message
+                    const data = JSON.parse(event.data);
+                    // wait for login success message
+                    if (data.type === "login") {
+                        const t3 = performance.now();
+                        console.log(`Server responded in ${t3 - t2}ms`);
+                        if (data.success) {
+                            // User exists and password is correct
+                            user = {
+                                id: data.id,
+                                username: data.username,
+                                role: data.role,
+                            };
+                            resolve(user);
+                        } else {
+                            // User exists but password is incorrect
+                            resolve(false);
+                        }
+                        result = data.success;
+                    }
+                };
+            });
 
-      const res = await loginPromise;
-      ws.close();
-      resolve(res);
+            const res = await loginPromise;
+            ws.close();
+            resolve(res);
+        });
+
+        ws.addEventListener("error", (event) => {
+            console.log(event);
+            reject(new Error("There was an error connecting to the webSocketServer"));
+        });
+
+        setTimeout(() => {
+            reject(new Error("The connection timed out"));
+        }, 5000);
     });
-
-    ws.addEventListener("error", (event) => {
-      console.log(event);
-      reject(new Error("There was an error connecting to the webSocketServer"));
-    });
-
-    setTimeout(() => {
-      if (!user && !result) {
-        reject(new Error("An unknown error occurred"));
-      }
-    }, 5000);
-  });
 }
 
 const authOptions: NextAuthOptions = {
-  session: {
-    strategy: "jwt",
-  },
-  providers: [
-    CredentialsProvider({
-      type: "credentials",
-      credentials: {},
-      async authorize(credentials) {
-        const { username, password } = credentials as {
-          username: string;
-          password: string;
-        };
-
-        // login logic
-        let success = await attempt_login(username, password);
-
-        if (typeof success !== "boolean") {
-          // login successful
-          return {
-            id: success.id,
-            name: success.username,
-            role: success.role,
-          };
-        } else {
-          // login failed
-          return null;
-        }
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/login",
-  },
-  callbacks: {
-    async jwt({ token, user }) {
-      // Set role property on token object
-      if (user) {
-        // @ts-ignore
-        token.role = user.role;
-      }
-      return token;
+    session: {
+        strategy: "jwt",
     },
-    async session({ session, token }) {
-      // Add role property to session object
-      // @ts-ignore
-      session.role = token.role;
-      return session;
+    providers: [
+        CredentialsProvider({
+            type: "credentials",
+            credentials: {},
+            async authorize(credentials) {
+                const {username, password} = credentials as {
+                    username: string;
+                    password: string;
+                };
+
+                const t0 = performance.now();
+                // login logic
+                let success = await attempt_login(username, password);
+                const t1 = performance.now();
+                console.log(`Authorized in ${t1 - t0}ms`);
+
+                if (typeof success !== "boolean") {
+                    // login successful
+                    return {
+                        id: success.id,
+                        name: success.username,
+                        role: success.role,
+                    };
+                } else {
+                    // login failed
+                    return null;
+                }
+            },
+        }),
+    ],
+    pages: {
+        signIn: "/login",
     },
-  },
-  secret: process.env.SECRET,
+    callbacks: {
+        async jwt({token, user}) {
+            // Set role property on token object
+            if (user) {
+                // @ts-ignore
+                token.role = user.role;
+            }
+            return token;
+        },
+        async session({session, token}) {
+            // Add role property to session object
+            // @ts-ignore
+            session.role = token.role;
+            return session;
+        },
+    },
+    secret: process.env.SECRET,
 };
 
 export default NextAuth(authOptions);
